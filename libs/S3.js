3bff8dd366d3ed0ea12009a1ded0a328124f15ce
ecarg-dev <hyperdev002@gmail.com>
2026-01-14 13:15:13 +0700

    kalo tidur jangan Malem - Malem

--- libs/S3.js
+++ libs/S3.js
@@ -5,12 +5,12 @@ import { configDotenv } from "dotenv";
 
 configDotenv()
 const s3 = new S3Client({
-    region: process.env.AWS_REGION,
+    region: process.env.AWS_REGION || 'us',
     endpoint: process.env.AWS_ENDPOINT,
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-    }
+    // credentials: {
+    //     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    //     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    // }
 })
 
 
